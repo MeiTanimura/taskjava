67c6abb2067d65bad2d313ecde0b56d42c99ac33
MeiTanimura <mei.t0919@gmail.com>
2023-08-02 19:18:28 +0900

    Qes1_3

--- HelloWorld_TanimuraMei/src/curriculum_B/Qes1_3.java
+++ HelloWorld_TanimuraMei/src/curriculum_B/Qes1_3.java
@@ -5,17 +5,16 @@ import java.util.Scanner;
 public class Qes1_3 {
 
 	public static void main(String[] args) {
-		String name = getName(); //ユーザー名を入力させる
-		String judgeName  = judgeNameMessage(name); //入力されたユーザー名が正しいか判断する
+		String name = getName(); //ユーザー名を入力させる。入力されたユーザー名は変数nameに格納する
+		String judgeName  = judgeNameMessage(name); //変数nameに格納されたユーザー名が正しいか判断する
 		int user = getUser(); //ユーザーの乱数を求める
 		int pc = getPc(); //PCの乱数を求める
 		String result = judgeJanken(user,pc); //求めた2つの乱数で勝ち、まけ、あいこを判断する。変数resultに結果を入れる
-		String pcJudgeMessage = judgeMessage(user,pc); //じゃんけんの結果によって出力されるメッセージを変数pcJudgeMessageに入れる。クラス名はjudgeMessage
+		String pcJudgeMessage = judgeMessage(user,pc); //じゃんけんの結果によって出力されるメッセージを変数pcJudgeMessageに入れる。
 		showResult(name,judgeName,user,pc,result,pcJudgeMessage);
 		}
 		
 	public static String getName() {
-		//ログイン時の入力チェックシステムを下記条件で作成してください
 		//コンソールにユーザー名を入力できるようにしてください
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("ユーザー名を入力してください");
@@ -26,16 +25,18 @@ public class Qes1_3 {
 		
   public static String judgeNameMessage(String name) {
 	  String judgeName  = "";
+	  /*ユーザー名の文字数が10文字より大きい場合「名前を10文字以内にしてください」と出力してください
+		ユーザー名の文字数が0文字以下もしくはnullの場合「名前を入力してください」と出力してください
+		ユーザー名が半角英数字以外の場合「半角英数字のみで名前を入力してください」と出力してください
+		ユーザー名が正常な値だった場合「ユーザー名「 入力したユーザー名 」を登録しました」と出力してください*/
 	  if(name.length()>10) {
 		  judgeName   ="名前を10文字以内にしてください";
 		}else if(name.length() <= 0 || name == null) {
 			judgeName    = "名前を入力してください";		
-		}else if(name.matches("^[A-Za-z0-9]+$")) {  
+		}else if(name.matches("^[A-Za-z0-9]+$")) {    //半角英数字以外とする方法の検討中
 			judgeName   = "半角英数字のみで名前を入力してください";	
 		}else {
-			judgeName  = "ユーザー名" + name + "を登録しました";
-			
-		
+			judgeName  = "ユーザー名" + name + "を登録しました";	
 
  }
 	return judgeName ;
@@ -43,12 +44,12 @@ public class Qes1_3 {
  
 	public static int getUser() {
 	Random rand = new Random();
-	return rand.nextInt(3);
+	return rand.nextInt(3); //乱数0,1,2を返す
 	}
 	
 	public static int getPc() {
 		Random rand = new Random();
-		return rand.nextInt(3);
+		return rand.nextInt(3);  //乱数0,1,2を返す
 		}
 	
 	public static String judgeJanken(int user,int pc) {
@@ -62,13 +63,12 @@ public class Qes1_3 {
 			result="あいこ";
 		}
 	 
-		// 勝敗結果を返す
-		return result;
+		return result; // 勝敗結果を返す
 		}
 		
 
 	public static String judgeMessage(int user,int pc) {
-		String pcJudgeMessage = "";
+		String pcJudgeMessage = ""; //勝ち、負け、あいこの場合に出力するメッセージを保存する
 		if((user == 0 && pc == 1) || (user == 1 && pc == 2)|| (user == 2 && pc == 0)) {
 			pcJudgeMessage = "やるやん。次は俺にリベンジさせて";	
 		}else if(user == 1 && pc == 0){
@@ -81,7 +81,7 @@ public class Qes1_3 {
 			pcJudgeMessage= "DRAW あいこ もう一回しましょう！";
 		}
 		
-		return pcJudgeMessage;
+		return pcJudgeMessage; //勝ち、負け、あいこの場合に出力するメッセージを返す
 	}
 
 
@@ -97,32 +97,9 @@ public class Qes1_3 {
 			break;}
 		}
 		
-			
-			
-			
-		/*ユーザー名の文字数が10文字より大きい場合「名前を10文字以内にしてください」と出力してください
-		ユーザー名の文字数が0文字以下もしくはnullの場合「名前を入力してください」と出力してください
-		ユーザー名が半角英数字以外の場合「半角英数字のみで名前を入力してください」と出力してください*/
-		/*if(userName.length()>10) {
-			System.out.println("名前を10文字以内にしてください");
-		}else if(userName.length() <= 0 || userName == null) {
-			System.out.println("名前を入力してください");		
-		}else if(userName != "^[0-9a-zA-Z]+$") {
-			System.out.println("半角英数字のみで名前を入力してください");	
-		}else {
-		System.out.println("ユーザー名" + userName + "を登録しました");
-		System.out.println(userName + "の手は" + janken[user]);
-		System.out.println("相手の手は" + janken[pc]);
-		System.out.println(result);*/
-		
-		
-		
-		
+
 }
 	}
 
-
-//}
-//while(user == 0 && pc == 2 || user == 1 && pc == 0 || user== 2 && pc == 1) {
 
 

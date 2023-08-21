76280f059b9e8ed3cda4c4e941220ef07e634822
MeiTanimura <mei.t0919@gmail.com>
2023-08-21 22:08:24 +0900

    Qes4,5 修正

--- HelloWorld_TanimuraMei/src/curriculum_B/Qes4.java
+++ HelloWorld_TanimuraMei/src/curriculum_B/Qes4.java
@@ -7,13 +7,11 @@ public class Qes4{
 		for(int i = 1 ; i < 10 ; i++) { // 外側のループ
 			for(int j = 1 ; j < 10 ; j++) { //内側のループ。内側のループが終わると１つの段が終わる。
 				int kuku = i * j; //変数kukuにi*jの結果を格納する
-				
-				if(kuku < 10) { //変数kukuの値が1桁の場合kukuの前に0を出力する。
-					System.out.print("||" +  "0" + i + "*" + "0" + j + "=" + "0" + kuku); 
-					
+				if(j == 9) {
+					System.out.print((String.format("%02d",i)) + "*" + (String.format("%02d",j)) + "="  + (String.format("%02d", kuku))); //2桁に揃えて出力 
 				}else {
-					System.out.print( "||" + "0" + i + "*" + "0" + j + "=" + kuku);
-				}
+				System.out.print((String.format("%02d",i)) + "*" + (String.format("%02d",j)) + "="  + (String.format("%02d", kuku)) + "||"); //行の始まりと終わり以外には段ごとの区切り線を出力する
+			}
 			}
 			System.out.println(" "); //内側のループが1周したら改行する。
 		} 
--- HelloWorld_TanimuraMei/src/curriculum_B/Qes5.java
+++ HelloWorld_TanimuraMei/src/curriculum_B/Qes5.java
@@ -8,8 +8,11 @@ public static void main(String[] args) {
 		for(int i = 1 ; i < 10 ; i++) { 
 			for(int j = 1 ; j < 21 ; j++) { 
 				int kuku = i * j; //変数kukuにi*jの結果を格納する
-				System.out.print("||"  + (String.format("%03d",j)) + "*" + (String.format("%03d",i)) + "="  + (String.format("%03d", kuku))); //3桁に揃えて出力
-	
+				if(j == 20) {
+					System.out.print((String.format("%03d",j)) + "*" + (String.format("%03d",i)) + "="  + (String.format("%03d", kuku))); //3桁に揃えて出力 
+				}else {
+				System.out.print((String.format("%03d",j)) + "*" + (String.format("%03d",i)) + "="  + (String.format("%03d", kuku)) + "||"); //行の始まりと終わり以外には段ごとの区切り線を出力する
+				}
 			}
 			System.out.println(" "); //内側のループが1周したら改行する。
 		}

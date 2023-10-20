b6d244feb32c36acef1ae71572927b7ed1b52ea2
MeiTanimura <mei.t0919@gmail.com>
2023-10-20 18:35:42 +0900

    Qes27 修正済

--- HelloWorld_TanimuraMei/src/qes27/Animal.java
+++ HelloWorld_TanimuraMei/src/qes27/Animal.java
@@ -3,20 +3,30 @@ package qes27;
 import java.util.Scanner;
 
 public class Animal {
+	public static String[] strArray;
 	String animalName;
 	String height;
 	String speed;
 	String scientificName;
 	
-	void animal(){
-		// 標準入力で得た文字列を「:」で分割して出力する
+	public void animal(){
+		// 標準入力で得た文字列を「,」で分割してstrArrayに格納する
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("コンソールに文字を入力してください");
-		String [] str = scanner.nextLine().split(":");
+		String str = scanner.nextLine();
+		String [] strArray = str.split(",");
 		scanner.close();
-		animalName = str[0];
-		height = str[1];
-		speed = str[2];
+		
+		// コンソールの入力値がない時にエラー文を出力する
+		 if (str.isEmpty()) {
+			 System.out.println("文字が入力されていません。入力してください。");
+		 }else {
+		//「,」で区切られた配列を再度「:」で区切り、標準入力された文字列を全て出力する
+	for(String anm : strArray) {
+		String [] strArray2 = anm.split(":");
+		animalName = strArray2[0];
+		height = strArray2[1];
+		speed = strArray2[2];
 		
 		// 標準入力された動物名（animalNameに格納）によって出力されるscientificNameを入力する
 		switch(animalName) {
@@ -43,8 +53,15 @@ public class Animal {
 		case "インコ":
 			scientificName ="不明";
 			break;
-		}
+			}
 		
+		System.out.println("動物名:" +animalName);
+		System.out.println("体長:" + height + "m");
+		System.out.println("速度:" + speed + "km/h");
+		System.out.println("学名:" +scientificName);
+		System.out.println("");
 	}
-}
+	 }
+	}
+    }
 
--- HelloWorld_TanimuraMei/src/qes27/Main.java
+++ HelloWorld_TanimuraMei/src/qes27/Main.java
@@ -7,10 +7,9 @@ public class Main {
 		// Animalクラスをインスタンス化してanimalメソッドを呼び出す
 		Animal animals = new Animal();
 		animals.animal();
-		System.out.println("動物名:" + animals.animalName);
-		System.out.println("体長:" + animals.height + "m");
-		System.out.println("速度:" + animals.speed + "km/h");
-		System.out.println("学名:" + animals.scientificName);
+		
+		
+		
 	}
 }
 

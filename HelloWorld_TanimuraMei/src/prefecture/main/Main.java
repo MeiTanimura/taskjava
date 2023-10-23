05a2ebc64bb500d2d8ec2620991de8b101380a89
MeiTanimura <mei.t0919@gmail.com>
2023-10-23 21:54:21 +0900

    Qes29修正

--- HelloWorld_TanimuraMei/src/prefecture/main/Main.java
+++ HelloWorld_TanimuraMei/src/prefecture/main/Main.java
@@ -1,14 +1,13 @@
 package prefecture.main;
+
 import prefecture.logics.Prefecture;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Prefecture prf = new Prefecture();
-		prf.prefecturedetail();	
+		prf.prefecturedetail();
 
 	}
 
 }
-
-

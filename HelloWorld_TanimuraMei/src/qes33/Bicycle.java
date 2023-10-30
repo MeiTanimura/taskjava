65bb649c50a82158dde9d76e4f0f32a55daf3671
MeiTanimura <mei.t0919@gmail.com>
2023-10-30 22:01:19 +0900

    Qes33

---
+++ HelloWorld_TanimuraMei/src/qes33/Bicycle.java
@@ -0,0 +1,6 @@
+package qes33;
+
+// 問題2：CarクラスとBicycleクラスを作成しましょう。
+public class Bicycle extends Vehicle{
+
+}
---
+++ HelloWorld_TanimuraMei/src/qes33/Car.java
@@ -0,0 +1,6 @@
+package qes33;
+
+// 問題2：CarクラスとBicycleクラスを作成しましょう。
+public class Car extends Vehicle{
+
+}
